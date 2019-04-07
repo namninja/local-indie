e348750fa1431d65e51588678d210842cebb38b9
Nam Ninja <namninja@gmail.com>
2019-04-06 19:16:53 -0700

    Added .env and updated keys

--- config/keys.js
+++ config/keys.js
@@ -1,19 +1,26 @@
-require("dotenv").config({ path: __dirname + "/.env" });
+require("dotenv").config({ path: "../.env" });
 const DATABASE = process.env.DATABASE;
 const TEST_DATABASE = process.env.TEST_DATABASE;
+const DEVDB = process.env.DEVDB;
+const DEVTESTDB = process.env.DEVTESTDB;
 const SECRET = process.env.SECRET;
 
+// DATABASE_URL:
+//   process.env.DATABASE_URL ||
+//   DATABASE,
+// TEST_DATABASE_URL:
+//   process.env.TEST_DATABASE_URL ||
+//   TEST_DATABASE,
+const DATABASE_URL = process.env.DATABASE_URL || DEVDB;
+const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || DEVTESTDB;
+const PORT = process.env.PORT || 8080;
+const JWT_SECRET = SECRET;
+const JWT_Expiry = 172800;
+
 module.exports = {
-  // DATABASE_URL:
-  //   process.env.DATABASE_URL ||
-  //   DATABASE,
-  // TEST_DATABASE_URL:
-  //   process.env.TEST_DATABASE_URL ||
-  //   TEST_DATABASE,
-  DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost/local-indie",
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL || "mongodb://localhost/test-local-indie",
-  PORT: process.env.PORT || 8080,
-  JWT_SECRET: SECRET,
-  JWT_Expiry: 172800
+  DATABASE_URL,
+  TEST_DATABASE_URL,
+  PORT,
+  JWT_SECRET,
+  JWT_Expiry
 };
