ec2e1ef5edea9bd2b1418e50a81ad9dcc1493a9a
Nam Ninja <namninja@gmail.com>
2019-04-06 17:25:19 -0700

    updating environment variables

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,19 @@
+require("dotenv").config({ path: __dirname + "/.env" });
+const DATABASE = process.env.DATABASE;
+const TEST_DATABASE = process.env.TEST_DATABASE;
+const SECRET = process.env.SECRET;
+
 module.exports = {
   // DATABASE_URL:
   //   process.env.DATABASE_URL ||
-  //   "mongodb+srv://zero-admin:zero-admin@cluster0-pfvwv.mongodb.net/local-indie?retryWrites=true",
+  //   DATABASE,
   // TEST_DATABASE_URL:
   //   process.env.TEST_DATABASE_URL ||
-  //   "mongodb+srv://zero-admin:zero-admin@cluster0-pfvwv.mongodb.net/test-local-indie?retryWrites=true",
+  //   TEST_DATABASE,
   DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost/local-indie",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL || "mongodb://localhost/test-local-indie",
   PORT: process.env.PORT || 8080,
-  JWT_SECRET: "wutangclanaintnothingtofuckwith",
+  JWT_SECRET: SECRET,
   JWT_Expiry: 172800
 };
